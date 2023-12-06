d24609f608e8767cbee1dd8e797c373055201c6e
Yan-pg <yancesa.07@gmail.com>
2023-12-06 18:57:13 -0300

    chore: configure colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,44 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        red: {
+          "primary-dark": "#B50010",
+          default: "#E2272A",
+          "primary-light": "#E46B6B",
+        },
+        yellow: {
+          "primary-dark": "#ffab00",
+          default: "#ffdd00",
+          "primary-light": "#FCF267",
+        },
+        error: {
+          "red-dark": "#DE3961",
+          default: "#F64F77",
+          "red-light": "#FC6F91",
+        },
+        gray: {
+          "100": "#1F1D2B",
+          "200": "#403e4d",
+          "300": "#5f5c6d",
+          "400": "#737081",
+          "500": "#9c99ab",
+          "600": "#bcb8cb",
+          "700": "#dfdbef",
+          "800": "#eeeafd",
+          "900": "#f5f2ff",
+          "1000": "#fbf7ff",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
