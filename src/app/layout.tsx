5e6cd21fc3ec1b846eb221c82f29257ceffa5e14
Yan-pg <yancesa.07@gmail.com>
2023-12-23 13:33:49 -0300

    feat: add login page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={`${outfit.className} ${rubik.className}`}>
-        {children}
+        <main>{children}</main>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,52 @@
 import { Button, TextFiled } from "@/modules/app/components";
+import { LockKey } from "@phosphor-icons/react/dist/ssr";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex gap-10">
-      <div className="min-w-[448px] flex gap-4 flex-col">
-        <Button>Button Primary</Button>
-        <Button variant="secondary">Button secondary</Button>
-        <Button variant="check">Button check</Button>
-        <Button variant="error">Button error</Button>
-        <Button disabled>Button disabled</Button>
-      </div>
+    <div className="h-screen">
+      <div className="flex justify-center items-center h-full">
+        <div className="flex flex-col items-center gap-10">
+          <Image
+            src="/logo.png"
+            alt="Picture of the author"
+            width={160}
+            height={76}
+          />
 
-      <div>
-        <TextFiled
-          htmlFor="name"
-          label="Name"
-          error="Preencha o carmpo corretamente"
-        >
-          <TextFiled.Input id="name" />
-        </TextFiled>
+          <div>
+            <div>
+              <h1 className="font-bold text-3xl font-rubik">
+                Bem-vindo- ao yeap!
+              </h1>
+              <p className="font-outfit font-medium text-neutral-500">
+                Preencha os campos abaixo e acesse sua conta
+              </p>
+            </div>
+
+            <div className="mt-5 flex flex-col gap-5">
+              <TextFiled htmlFor="email" label="E-mail ou celular" error={null}>
+                <TextFiled.Input id="email" placeholder="email@email.com" />
+              </TextFiled>
+              <TextFiled htmlFor="password" label="Senha" error={null}>
+                <TextFiled.Input id="password" placeholder="Crie sua senha" />
+              </TextFiled>
+
+              <Button>Entrar</Button>
+            </div>
+
+            <div className="flex items-center justify-center">
+              <button className="flex justify-center mt-5 items-center gap-2">
+                <LockKey size={20} weight="regular" color="#B50010" />
+
+                <p className="font-outfit font-medium text-gray-100">
+                  Esqueci minha senha
+                </p>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
