de6f062e78ac8bb4f7b17e406f62a833fe960eee
Yan-pg <yancesa.07@gmail.com>
2024-01-03 15:47:12 -0300

    refactor: refactor input

--- src/modules/app/components/Input/index.tsx
+++ src/modules/app/components/Input/index.tsx
@@ -1,28 +1,94 @@
+"use client";
+
 import { VariantProps, tv } from "tailwind-variants";
-import { Filed } from "../Filed";
-import { forwardRef } from "react";
+import { ReactNode, forwardRef, useState } from "react";
+import InputMask from "react-input-mask";
 
 const input = tv({
   slots: {
     container: [
-      "border border-gray-800 h-10 rounded-xl w-full",
-      "bg-gray-1000 px-2",
-      "focus:border-[#7B58FF] outline-none",
+      "border border-gray-700 h-10 rounded-xl w-full",
+      "bg-gray-1000 flex items-center gap-1.5 px-2",
       "font-inter text-sm",
-      "placeholder:text-gray-500 placeholder:text-xs",
     ],
+    inputStyle:
+      "w-full min-h-full bg-transparent outline-none placeholder:text-gray-500 placeholder:text-xs",
+  },
+
+  variants: {
+    isFocus: {
+      true: {
+        container: ["border-[#7B58FF]"],
+      },
+    },
   },
 });
 
 interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement>,
-    VariantProps<typeof input> {}
+    VariantProps<typeof input> {
+  startIcon?: ReactNode;
+  endIcon?: ReactNode;
+  mask?: string;
+  onInputFocus?(): void;
+  onInputBlur?(): void;
+  onInputClick?(): void;
+}
 
-const Input = forwardRef<HTMLInputElement, InputProps>(({ ...props }, ref) => {
-  const { container } = input();
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      startIcon,
+      endIcon,
+      mask,
+      onInputFocus,
+      onInputBlur,
+      onInputClick,
+      ...props
+    },
+    ref
+  ) => {
+    const [isFocus, setIsFocus] = useState(false);
+    const { container, inputStyle } = input({ isFocus });
 
-  return <input type="text" {...props} className={container()} ref={ref} />;
-});
+    function handleFocus() {
+      setIsFocus(true);
+      onInputFocus && onInputFocus();
+    }
+
+    function handleBlur() {
+      setIsFocus(false);
+      onInputBlur && onInputBlur();
+    }
+
+    function handleClick() {
+      console.log(onInputClick);
+
+      onInputClick && onInputClick();
+    }
+
+    return (
+      <div
+        data-cy="container-input"
+        className={container()}
+        onClick={handleClick}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      >
+        {startIcon && startIcon}
+        <InputMask
+          mask={mask}
+          maskPlaceholder=""
+          type="text"
+          {...props}
+          className={inputStyle()}
+          ref={ref}
+        />
+        {endIcon && endIcon}
+      </div>
+    );
+  }
+);
 
 Input.displayName = "Input";
 
